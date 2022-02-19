72cd787ca308680dbe590a424e00c1a392a1b0bb
satyamsingh07 <satyamsingh20027@gmail.com>
2022-02-20 03:05:49 +0530

    pages made

---
+++ client/src/components/SignIn/Signin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from '../Form/Form.jsx'
+
+function Signin() {
+  return (
+    <Form  name ='Sign In' btnName="Sign Up" text="Don't have an account?" /> //implement function
+  )
+}
+
+export default Signin
+
