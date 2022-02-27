f0a747c2bc2b9bc334201d3495d97cb57cdb8b4c
Swaathy Kumar <72341529+tinycoder2@users.noreply.github.com>
2022-02-27 21:05:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // connect to mongodb & listen for requests
 // const dbURI = "mongodb+srv://netninja:test1234@net-ninja-tuts-del96.mongodb.net/node-tuts";
-const dbURI = 'mongodb+srv://swaathy:test1@trialcluster.2c3bh.mongodb.net/trial?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://username:pwd@trialcluster.2c3bh.mongodb.net/trial?retryWrites=true&w=majority';
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(result => app.listen(3000))
